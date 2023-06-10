827bc624bbc87e9626801611c596bbf3431c0c52
Brandom_Vega <93046780+BrandomVega@users.noreply.github.com>
2023-06-10 18:50:42 -0400

    Add files via upload

---
+++ login.js
@@ -0,0 +1,56 @@
+$(document).ready(()=>{
+    const validator =  new JustValidate("#loginform");
+    validator.addField('#numeroEmpleado', [
+        {
+        rule: 'required',
+        errorMessage:"Por favor ingresa tu usuario"
+        },
+        {
+        rule: 'integer',
+        errorMessage:"Deben ser solo números"
+        },
+        {
+        rule: 'maxLength',
+        value: 3,
+        errorMessage:"Maximo 3 digitos"
+        },
+    ])
+    .addField('#contrasena', [
+        {
+        rule: 'required',
+        errorMessage:"Falta tu contraseña"
+        },
+        {
+        rule: 'integer',
+        },
+        {
+        rule: 'maxLength',
+        value: 3,
+        },
+    ]).onSuccess(()=>{
+      $.ajax({
+        url:"../php/index_AX.php",
+        method:"POST",
+        data:$("#loginform").serialize(),
+        cache:false,
+        success:(respAX)=>{
+          let AX = JSON.parse(respAX);
+          Swal.fire({
+            title:"ESCOM - IPN",
+            text:AX.msj,
+            icon:AX.icono,
+            didDestroy:()=>{
+              if(AX.cod == 1){
+                location.href = "../Matform.html";
+              }else if(AX.cod == 2){
+                location.href = "../admin.html";
+              }else{
+                location.reload();
+              }  
+            }
+          }); // sweetAlert/
+        }
+      }); // ajax/
+    }); // justValidate/
+}); // ready/
+
